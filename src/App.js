3a7e286f451f2d09915eb2cebf3be73d744fb9f5
J7477 <67478939+J7477@users.noreply.github.com>
2022-01-15 00:29:50 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <>Hello world</>
+    <>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+      </Routes>
+    </>
   )
 }
 
