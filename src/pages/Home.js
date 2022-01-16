ca533c970524bf37b9ac70b93de0438db1d85db2
J7477 <67478939+J7477@users.noreply.github.com>
2022-01-17 03:29:19 +0530

    tried adding login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,10 +9,56 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 
 const Home = () => {
+
+    const initialValues = {
+        username: "",
+        password: "",
+    }
+
+
+    const onSubmit = (values) => {
+
+        const requestBody = {
+            query: `
+            mutation{
+                login(input:{email:"${values.username}",password:"${values.username}"}){
+                    userId
+                    token
+                }
+            }
+            `
+        }
+
+        fetch('http://localhost:4000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (res) {
+                    return res.json();
+                }
+            })
+            .then(resData => {
+                console.log(resData)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+    }
+    // .email("Please Enter a valid Email").
+    const validationSchema = Yup.object().shape({
+        username: Yup.string().required("Please Enter an Email"),
+        password: Yup.string().min(2, 'Password Must Have More Than 2 Characters').required('Please Enter Your Password')
+    })
 
 
     return (
@@ -33,46 +79,55 @@ const Home = () => {
                 <Typography component="h1" variant="h5">
                     Sign in
                 </Typography>
-                <Box component="form" noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Sign In
-                    </Button>
-                    <Grid container>
-
-                        <Grid item>
-                            <Link href="#" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </Box>
             </Box>
+
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+                {(props) => (
+                    <Form>
+                        <Field as={TextField}
+                            margin="normal"
+                            // required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="username"
+                            autoComplete="email"
+                            autoFocus
+                            helperText={<ErrorMessage name='username' />}
+                        />
+                        <Field as={TextField}
+                            margin="normal"
+                            // required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            helperText={<ErrorMessage name='password' />}
+                        />
+
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+
+
+                        <Grid container>
+                            <Grid item>
+                                <Button color="primary" component={Link} to={"/userSignup"}>Don't have an account? Sign up</Button>
+                            </Grid>
+                            <Grid>
+                                <Button color="primary" component={Link} to={"/"}>Home Page</Button>
+                            </Grid>
+                        </Grid>
+                    </Form>
+                )}
+            </Formik>
 
         </Container>
 
